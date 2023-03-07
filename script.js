85881fee6917fe174805346929d0cd849f46278b
amaranand360 <aknbo30@gmail.com>
2023-03-08 00:12:58 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const searchfun = () =>{
+const searchbox = document.getElementById("search-item").ariaValueMax.toUpperCase();
+const storeitems = document.getElementById("product-list")
+const product = document.querySelectorAll(".product")
+const pname = storeitems.getElementsByTagName("h3")
+
+for(let i=0;i<pname.length;i++){
+    let match = product[i].getElementsByTagName('h3')[0];
+    
+    if(match) {
+        let textvalue = match.textContent || match.innerHTML
+
+        if(textvalue.toUpperCase().indexOf(searchbox) > -1){
+            product[i].style.display = " ";
+
+        }
+        else{
+            product[i].style.display ="none"
+        }
+    }
+
+}
+}
